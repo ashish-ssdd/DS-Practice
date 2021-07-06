0dd85bd8e042cdbda012eadfc8632cdef3d91db6
ashish-ssdd <77218946+ashish-ssdd@users.noreply.github.com>
2021-07-06 10:42:54 +0530

    Create ReverseLL.java
    Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
    Input: head = [1,2,3,4,5], left = 2, right = 4
    Output: [1,4,3,2,5]

---
+++ LinkedList/ReverseLL.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode sHead = new ListNode(),sTail = sHead,rHead = null, rTail = null, node = head;
+        int i= 0; 
+        
+        while(node!= null){//Still have nodes to process
+            //Storing next node before doing any operation
+            ListNode next = node.next;
+            //disconnecting current node
+            node.next = null;
+            i++;
+            
+            if(i<left || i> right){//not in the range where we want to reverse
+                sTail.next = node;
+                sTail = node;
+            } 
+            else{//in the range
+                
+//The first node in the range we encounter will become tail of the reversed list
+                if(rTail == null)
+                    rTail = node;
+                
+                //making the current node head of the reversed linkedList
+                node.next = rHead;
+                rHead = node;
+                //if This is the last node of the reverse linked list
+                if(i+1> right){
+            //making the node before the reversed list point to the head of reversed node    
+                    sTail.next = rHead;
+                    //Setting stail = rtail so that in next iteration we append as usual 
+                    sTail = rTail;
+                }
+            }
+            node = next;
+        }
+        return sHead.next;
+    }
+}
