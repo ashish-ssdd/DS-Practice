9626bed91fbcf808d34d7c0722a681f0a2ae6abf
ashish-ssdd <77218946+ashish-ssdd@users.noreply.github.com>
2021-07-06 11:06:41 +0530

    Update RemoveElements.java

--- LinkedList/RemoveElements.java
+++ LinkedList/RemoveElements.java
@@ -12,18 +12,27 @@
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         ListNode node = head;
+        //previous node initially null
         ListNode prev =null;
         while(node!=null){
+            //Storing next node 
             ListNode temp = node.next;
             if(node.val == val){
+                //prev is not null i.e. element to remove is not at head
                 if(prev!=null)
                      prev.next = temp;
+                //element to remove is at head so move head forward
                 else
                     head = temp;
+                //disconnect the node to delete
                 node.next =null;
+                //now the node is pointing to the previous node
                 node = prev;
             }
+            //if no changes were made prev will point to next node 
+            //if any node was deleted prev will point to itself as we have changed node to prev
             prev= node;
+            //moving node forward using the next node we stored earlier
             node = temp;
         }
         return head;
